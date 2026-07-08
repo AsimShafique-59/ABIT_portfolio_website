8b0deefbf20c10820ed31fe994b2dd923e6b2dcc
AsimShafique-59 <asimshafique59@gmail.com>
2026-07-08 20:54:40 +0500

    remove unused files

--- apps/api/src/shared/middleware/logger.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-export function loggerMiddleware(req: Request, res: Response, next: NextFunction): void {
-  const start = Date.now()
-  res.on('finish', () => {
-    console.log(`${req.method} ${req.path} ${res.statusCode} ${Date.now() - start}ms`)
-  })
-  next()
-}
